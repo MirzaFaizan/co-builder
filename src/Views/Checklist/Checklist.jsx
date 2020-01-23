27513529b91a07e7736b8c338ea8ed6c8b7d9fc5
irtiza007 <irtizahassan58@outlook.com>
2020-01-23 09:53:34 +0500

    checklist user side added :start:

--- src/Views/Checklist/Checklist.jsx
+++ src/Views/Checklist/Checklist.jsx
@@ -1,65 +1,77 @@
-import React, {useState} from 'react';
-import Model from './Component/Model/Model';
-import ProgressBar from './Component/Progressbar';
-import MonthBasedChecklist from './Component/MonthBasedChecklist/Index';
-export default function CheckList(){
+import React, { useState } from "react";
+import Model from "./Component/Model/Model";
+import ProgressBar from "./Component/Progressbar";
+import MonthBasedChecklist from "./Component/MonthBasedChecklist/Index";
+import Api from "../../ApiCalls/api";
+import "./checklist.css";
+export default function CheckList(props) {
   const [openModel, setOpenModel] = useState(false);
- const ToggleModel = () => {
-  setOpenModel(!openModel);
- }
- 
-    return(
-        <div className="main-testing bg-white p-4">
-   <Model openModel={openModel} setOpenModel={setOpenModel} />
-  
-        <div>
-          
-          <div className="d-flex flex-column flex-md-row justify-content-between align-items-center align-items-lg-baseline">
-           <h3 className="mb-0"><span className="custom-text-gold font-weight-bold">427</span> Days to go</h3>
-           
-           <div className="text-center">
-            <button className="btn btn-outline-info btn-round">Edit Checklist</button>
-            <button className="btn btn-info btn-round">Create New To-Do</button>
-           </div>
+  const [type, setType] = useState("new");
+  const ToggleModel = () => {
+    setType("");
+    setOpenModel(!openModel);
+  };
+  const body = {
+    userid: localStorage.getItem("userId")
+  };
+
+  // React.useEffect(() => {
+  //   Api.getChecklist(body)
+  //     .then(res => {
+  //       console.log(res);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
+  return (
+    <div className="main-testing bg-white p-4">
+      <Model openModel={openModel} setOpenModel={setOpenModel} type={type} />
+      <div>
+        <div className="d-flex flex-column flex-md-row justify-content-between align-items-center align-items-lg-baseline">
+          <h3 className="mb-0">
+            <span className="custom-text-gold font-weight-bold">427</span> Days
+            to go
+          </h3>
+
+          <div className="text-center">
+            {/* <button className="btn btn-outline-info btn-round">
+              Edit Checklist
+            </button> */}
+            <button
+              className="btn btn-info btn-round"
+              onClick={() => {
+                setType("new");
+                setOpenModel(!openModel);
+              }}
+            >
+              Create New To-Do
+            </button>
           </div>
-   <ProgressBar />
-          
-   
-         </div>
-   
-        
-         
-   
-       {/* <!-- History-Section-Start  --> */}
-        <div className="history-section">
-         
+        </div>
+        <ProgressBar />
+      </div>
+
+      {/* <!-- History-Section-Start  --> */}
+      <div className="history-section">
         {/* <!-- Toggle-Button-Start --> */}
         <div className="row mr-0 flex-column flex-md-row align-items-center justify-content-between">
-         <div className="h4 col text-center text-md-left mb-0 mt-3 font-weight-bold"></div>
-        
-         <div className="col-7 col-md-5 col-lg-4 col-xl-3 p-1 mt-2 mt-md-0 text-capitalize border">
-          
-           <small className="text-muted ml-1">View By</small>
-           
-           <select className="form-control selectdiv p-0 border-0 font-weight-bold">
-             <option selected="">All Months</option>
-             <option>...</option>
-           </select>
-          
-         </div>
-         
-        </div>
-         {/* <!-- Toggle-Button-End --> */}
-   
-         {/* <!-- Toggle-Here-Start -->   */}
-        <div>
-          {/* <!-- History-Section-1  --> */}
-         <MonthBasedChecklist
-         ToggleModel={ToggleModel}
-         />
+          <div className="h4 col text-center text-md-left mb-0 mt-3 font-weight-bold"></div>
+
+          <div className="col-7 col-md-5 col-lg-4 col-xl-3 p-1 mt-2 mt-md-0 text-capitalize border">
+            <small className="text-muted ml-1">View By</small>
+
+            <select className="form-control selectdiv p-0 border-0 font-weight-bold">
+              <option selected="">All Months</option>
+              <option>...</option>
+            </select>
           </div>
-      {/* <!-- History-Section-End  --> */}
+        </div>
+        <div>
+          <MonthBasedChecklist ToggleModel={ToggleModel} />
         </div>
       </div>
-    )
+    </div>
+  );
 }
--- src/Views/Checklist/Component/Model/Model.jsx
+++ src/Views/Checklist/Component/Model/Model.jsx
@@ -1,107 +1,224 @@
-import React from 'react';
-import Calender from '../Calender';
-import time from '../../../../Helpers/DateHelpers';
-import './model.css';
-export default function Model({openModel, setOpenModel}){
-    const [Right, setRight] = React.useState('-45%')
-    const [Description, setDescription ] = React.useState('Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt cond')
-   const [title, setTitle] = React.useState('Upload House Plan');
-    const [date, setDate]= React.useState(new Date());
-    const onChange = date => {
-      setDate(date);
-    }  
- if(openModel === true){
-   if(Right === '-45%')
-   setTimeout(()=>{
-    setRight('0px');
-   }, 100);
- }
+import React from "react";
+import Calender from "../Calender";
+import time from "../../../../Helpers/DateHelpers";
+import "./model.css";
+export default function Model({ openModel, setOpenModel, type }) {
+  const [Right, setRight] = React.useState("-45%");
+  const [Description, setDescription] = React.useState(
+    "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt cond"
+  );
+  const [title, setTitle] = React.useState("Upload House Plan");
+  const [date, setDate] = React.useState(new Date());
+  const onChange = date => {
+    setDate(date);
+  };
+  if (openModel === true) {
+    if (Right === "-45%")
+      setTimeout(() => {
+        setRight("0px");
+      }, 100);
+  }
 
-
-    return(
+  return (
+    <>
+      {openModel ? (
         <>
-        {openModel ? (
-            
-<>
-<div className="container demo model-swipe">
-	<div className="modal right fade" aria-labelledby="myModalLabel2">
-		<div className="model-dialog-custom" style={{right:Right}}>
-    <div className="modal-content">
-                <div className="modal-header bg-white">
-                  <div className="col-sm-8 text-right">
-                  <input className="modal-title h3 border-0 mb-0" value={title} onChange={(e)=>setTitle(e.target.value)} />
-                  <small className="text-blue"> Click to edit</small>
+          <div className="container demo model-swipe">
+            <div className="modal right fade" aria-labelledby="myModalLabel2">
+              <div className="model-dialog-custom" style={{ right: Right }}>
+                {type === "new" ? (
+                  <div className="modal-content">
+                    <div className="modal-header bg-white">
+                      <div className="col-sm-8">
+                        <h3 className="ml-3 mb-1">Create New Todo-List</h3>
+                      </div>
+                      <button
+                        type="button"
+                        className="close"
+                        onClick={() => {
+                          setRight("-45%");
+                          setOpenModel(false);
+                        }}
+                      >
+                        <span>&times;</span>
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <div className="col-sm-12 d-flex align-items-center pb-2">
+                        <div className="col-sm-3">
+                          <span className="font-weight-bold mr-3">Title:</span>
+                        </div>
+                        <div className="col-sm-8 font-weight-bold p-1 text-capitalize border btn-light cursor-pointer">
+                          <input type="text" />
+                        </div>
+                      </div>
+                      <div className="col-sm-12 d-flex align-items-center">
+                        <div className="col-sm-3">
+                          <span className="font-weight-bold mr-3">
+                            Due Date:
+                          </span>
+                        </div>
+                        <div className="col-sm-8 font-weight-bold p-1 text-capitalize border btn-light cursor-pointer">
+                          {time.getDate(date)}
+                        </div>
+                      </div>
+                      <div className="col-sm-12 d-flex pt-2 align-items-center">
+                        <div className="col-sm-3"></div>
+                        <div className="col-sm-8 p-0">
+                          <Calender date={date} onChange={onChange} />
+                        </div>
+                      </div>
+
+                      <div className="col-sm-12 d-flex align-items-center pt-2 ">
+                        <div className="col-sm-3 text-right">
+                          <span className="font-weight-bold mr-3">
+                            Discripton:
+                          </span>
+                        </div>
+                        <div className="col-sm-8 p-0 text-capitalize">
+                          <textarea
+                            placeholder="Enter description"
+                            className="h-100 w-100 font-weight-400"
+                            rows="3"
+                          ></textarea>
+                        </div>
+                      </div>
+                      <div className="col-sm-12 d-flex align-items-center pb-2">
+                        <div className="col-sm-3">
+                          <span className="font-weight-bold mr-3">Status:</span>
+                        </div>
+                        <div className="col-sm-8 cursor-pointer">
+                          <div
+                            className="form-check"
+                            style={{ marginLeft: "3.2rem" }}
+                          >
+                            <label className="form-check-label">
+                              <input
+                                className="form-check-input"
+                                type="checkbox"
+                              />
+                              <span className="form-check-sign custom-bg-gold"></span>
+                              <h5 className="m-0">Mark it done</h5>
+                              <p className="text-black-50">
+                                <span className="custom-text-gold">
+                                  leaves Uncheck if it's pending
+                                </span>{" "}
+                              </p>
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className="container text-right"
+                      style={{ maxWidth: "75.6%" }}
+                    >
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                        onClick={() => {
+                          setRight("-45%");
+                          setOpenModel(false);
+                        }}
+                      >
+                        Save changes
+                      </button>
+                    </div>
                   </div>
-                  <button type="button" className="close" 
-                  onClick={()=>{
-                  setRight('-45%');
-                  setOpenModel(false);
-                  }}>
-                    <span>&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                <div className="form-check" style={{marginLeft:'3.2rem'}}>
-             <label className="form-check-label">
-               <input className="form-check-input" type="checkbox" />
-               <span className="form-check-sign custom-bg-gold"></span>
-                 <h5 className="m-0">Mark it done</h5>
-                 <p className="text-black-50"><span className="custom-text-gold">Past Due :</span> Due December 11</p>
-              
-             </label>
-           </div>
-                  <div className="col-sm-12 d-flex align-items-center">
-                    <div className="col-sm-3 text-right">
-                      <span className="font-weight-bold mr-3">
-                        Due Date:
-                      </span>
+                ) : (
+                  <div className="modal-content">
+                    <div className="modal-header bg-white">
+                      <div className="col-sm-8 text-right">
+                        <input
+                          className="modal-title h3 border-0 mb-0"
+                          value={title}
+                          onChange={e => setTitle(e.target.value)}
+                        />
+                        <small className="text-blue"> Click to edit</small>
                       </div>
-                  <div className="col-sm-4 font-weight-bold p-1 text-capitalize border btn-light cursor-pointer">
-                      {time.getDate(date)}
-        </div>
-        </div>
-    <div className="col-sm-12 d-flex pt-2 align-items-center">
-      <div className="col-sm-3">
-       
-      </div>
-      <div className="col-sm-8 pl-0">
-    <Calender
-     date={date}
-     onChange={onChange}
-     />
-     </div>
-     </div>
-
-        <div className="col-sm-12 d-flex align-items-center pt-2 ">
-          <div className="col-sm-3 text-right">
-                      <span className="font-weight-bold mr-3">
-                        Discripton:
-                      </span>
+                      <button
+                        type="button"
+                        className="close"
+                        onClick={() => {
+                          setRight("-45%");
+                          setOpenModel(false);
+                        }}
+                      >
+                        <span>&times;</span>
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <div
+                        className="form-check"
+                        style={{ marginLeft: "3.2rem" }}
+                      >
+                        <label className="form-check-label">
+                          <input className="form-check-input" type="checkbox" />
+                          <span className="form-check-sign custom-bg-gold"></span>
+                          <h5 className="m-0">Mark it done</h5>
+                          <p className="text-black-50">
+                            <span className="custom-text-gold">Past Due :</span>{" "}
+                            Due December 11
+                          </p>
+                        </label>
                       </div>
-                  <div className="col-sm-8 pl-0 text-capitalize">
-                  <textarea className="h-100 w-100 font-weight-400" rows="4" value={Description} onChange={(e)=>{setDescription(e.target.value)}}></textarea> 
-        </div>
-        </div>
+                      <div className="col-sm-12 d-flex align-items-center">
+                        <div className="col-sm-3 text-right">
+                          <span className="font-weight-bold mr-3">
+                            Due Date:
+                          </span>
+                        </div>
+                        <div className="col-sm-4 font-weight-bold p-1 text-capitalize border btn-light cursor-pointer">
+                          {time.getDate(date)}
+                        </div>
+                      </div>
+                      <div className="col-sm-12 d-flex pt-2 align-items-center">
+                        <div className="col-sm-3"></div>
+                        <div className="col-sm-8 pl-0">
+                          <Calender date={date} onChange={onChange} />
+                        </div>
+                      </div>
 
-        
-             
-                </div>
-                <div className="container text-right" style={{maxWidth:'75.6%'}}>
-                  <button type="button" class="btn btn-primary" onClick={()=>{
-                   setRight('-45%');
-                   setOpenModel(false);}
-                  }>Save changes</button>
-                </div>
+                      <div className="col-sm-12 d-flex align-items-center pt-2 ">
+                        <div className="col-sm-3 text-right">
+                          <span className="font-weight-bold mr-3">
+                            Discripton:
+                          </span>
+                        </div>
+                        <div className="col-sm-8 pl-0 text-capitalize">
+                          <textarea
+                            className="h-100 w-100 font-weight-400"
+                            rows="4"
+                            value={Description}
+                            onChange={e => {
+                              setDescription(e.target.value);
+                            }}
+                          ></textarea>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className="container text-right"
+                      style={{ maxWidth: "75.6%" }}
+                    >
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                        onClick={() => {
+                          setRight("-45%");
+                          setOpenModel(false);
+                        }}
+                      >
+                        Save changes
+                      </button>
+                    </div>
+                  </div>
+                )}
               </div>
-
-			</div>
-		</div>
-	</div>
-	
-	
-
-
-</>
-        ) : null}
-</>
-    )}
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+}
